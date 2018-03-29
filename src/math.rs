b0a261f327b65e061b1fa0c6e072462f67960bf2
yinyanlv <yanlv.yin@gmail.com>
2018-03-29 15:38:12 +0800

    增加test

--- src/math.rs
+++ src/math.rs
@@ -35,3 +35,33 @@ pub fn concat_number(a: i32, b: i32) -> i32 {
 
     i32::from_str(&*(a.to_string() + &*b.to_string())).unwrap()
 }
+
+#[test]
+fn test_add() {
+    assert_eq!(add(1, 2), 3);
+}
+
+#[test]
+fn test_subtract() {
+    assert_eq!(subtract(1, 2), -1);
+}
+
+#[test]
+fn test_multiply() {
+    assert_eq!(multiply(1, 2), 2);
+}
+
+#[test]
+fn test_divide() {
+    assert_eq!(divide(1, 2), 0);
+}
+
+#[test]
+fn test_modulo() {
+    assert_eq!(modulo(1, 2), 1);
+}
+
+#[test]
+fn test_concat_number() {
+    assert_eq!(concat_number(1, 2), 12);
+}
