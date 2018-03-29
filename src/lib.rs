9249dbeba30daf0b07f8026ad1cb88835506b9de
yinyanlv <yanlv.yin@gmail.com>
2018-03-29 15:25:52 +0800

    修改lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,42 @@
 //! rustup target add wasm32-unknown-unknown
 //! rustc +stable --target wasm32-unknown-unknown -O ./src/lib.rs --out-dir ./target --crate-type=cdylib
-//! cargo install --git https://github.com/alexcrichton/wasm-gc
+//! cargo install wasm-gc
 //! wasm-gc ./target/lib.wasm ./target/lib.min.wasm
 
+use std::str::FromStr;
+
 #[no_mangle]
-pub extern fn add(a: i32, b: i32) -> i32 {
+pub fn add(a: i32, b: i32) -> i32 {
 
     a + b
 }
 
 #[no_mangle]
-pub extern fn subtract(a: i32, b: i32) -> i32 {
+pub fn subtract(a: i32, b: i32) -> i32 {
 
     a - b
 }
 
 #[no_mangle]
-pub extern fn multiply(a: i32, b: i32) -> i32 {
+pub fn multiply(a: i32, b: i32) -> i32 {
 
     a * b
 }
 
 #[no_mangle]
-pub extern fn divide(a: i32, b: i32) -> i32 {
+pub fn divide(a: i32, b: i32) -> i32 {
 
     a / b
+}
+
+#[no_mangle]
+pub fn modulo(a: i32, b: i32) -> i32 {
+
+    a % b
+}
+
+#[no_mangle]
+pub fn concat_number(a: i32, b: i32) -> i32 {
+
+    i32::from_str(&*(a.to_string() + &*b.to_string())).unwrap()
 }
